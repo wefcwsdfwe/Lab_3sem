5568aa8a1915729bc87f3d05c023979e4abdd474
enikeev00 <maksimenikeev03@mail.ru>
2022-09-23 17:32:54 +0600

    append Move and Rotate(first version in methods)

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ namespace consoleApplication1
 {
     public class Tank
     {
-        public int x;
-        public int y;
+        private double x;
+        private double y;
+        private double angle;
         
         public Tank(int x, int y)
         {
@@ -15,24 +16,60 @@ namespace consoleApplication1
             this.y = y;
         }
 
-        public void SetCoordinates(int x, int y)
+        public void Move(int NewCordinate_X, int NewCordinate_Y)
         {
-            this.x = x;
-            this.y = y;
+           
+            if (this.angle != 0)
+            {
+                double New_X, New_Y;
+                New_X = NewCordinate_X * Math.Cos(angle);
+                New_Y = NewCordinate_Y * Math.Sin(angle);
+                
+                this.x += New_X;
+                this.y += New_Y;
+                this.angle = 0;
+            }
+            else
+            {
+                this.x += NewCordinate_X;
+                this.y += NewCordinate_Y;
+            }
+            Console.WriteLine($"{"("} {this.x} {";"} {this.y} {")"}");
         }
 
-        public int Manevr(Tank T)
+        public void Rotate(double angle)
         {
-            // if (...) {x+-1}
-            // if (...){y+-1}
+            // double New_X, New_Y;
+            // New_X = x * Math.Cos(angle);
+            // New_Y = y * Math.Sin(angle);
+            //
+            // this.x = New_X;
+            // this.y = New_Y;
+            //
+            // Console.WriteLine($"{"("} {this.x} {";"} {this.y} {")"}");
+            bool flag = true;
+            if (flag == false)
+            {
+                this.angle = angle;
+            }
+            else
+            {
+                this.angle = angle;
+                flag = false;
+            }
         }
 
         class Program
         {
             static void Main(string[] args)
             {
-                Tank T34 = new Tank();
-                T34.SetCoordinates(0, 0);
+                Tank T34 = new Tank(0, 0);
+                T34.Move(2, 3);
+                T34.Move(-1, 2);
+                T34.Rotate(Math.PI/4);
+                T34.Move(1, 8);
+                T34.Move(2, 3);
+                
             }
         }
     }
